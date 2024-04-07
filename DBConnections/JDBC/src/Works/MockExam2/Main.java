55ecc538d0664a850a0c549562677381b43041de
JhonatanTH7 <jhonatantorohurtado93@gmail.com>
2024-04-06 23:31:48 -0500

    Creation of menus

--- DBConnections/JDBC/src/Works/MockExam2/Main.java
+++ DBConnections/JDBC/src/Works/MockExam2/Main.java
@@ -1,12 +1,44 @@
 package Works.MockExam2;
 
-import Works.MockExam2.database.ConfigDB;
-
-import java.sql.Connection;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        Connection objConnection = ConfigDB.openConnection();
-        ConfigDB.closeConnection();
+        Menus menus = new Menus();
+        String option;
+        do {
+            option = JOptionPane.showInputDialog(null, """
+
+                           MAIN MENU
+
+                    1. Manage Planes
+                    2. Manage Flights
+                    3. Manage Passengers
+                    4. Manage Reservations
+                    5. Exit
+
+                    Choose an option:
+
+                    """);
+            switch (option) {
+                case "1":
+                    menus.planeMenu();
+                    break;
+                case "2":
+                    menus.flightMenu();
+                    break;
+                case "3":
+                    menus.passengerMenu();
+                    break;
+                case "4":
+                    menus.reservationMenu();
+                    break;
+                case "5":
+                    JOptionPane.showMessageDialog(null, "See you next time!!!");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Not a valid option");
+            }
+        } while (!option.equals("5"));
     }
 }
--- DBConnections/JDBC/src/Works/MockExam2/Menus.java
+++ DBConnections/JDBC/src/Works/MockExam2/Menus.java
@@ -1,4 +1,158 @@
 package Works.MockExam2;
 
+
+import javax.swing.*;
+
 public class Menus {
+    public void planeMenu() {
+        String option2;
+        do {
+            option2 = JOptionPane.showInputDialog(null, """
+
+                        SPECIALTIES MENU
+
+                    1. Show all Specialties
+                    2. Add Specialty
+                    3. Update Specialty
+                    4. Delete Specialty
+                    5. Show all Physicians in a Specialty
+                    6. Exit
+
+                    Choose an option:
+
+                    """);
+            switch (option2) {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    JOptionPane.showMessageDialog(null, "Going back to main menu");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Not a valid option");
+                    break;
+            }
+        } while (!option2.equals("6"));
+    }
+
+    public void flightMenu() {
+        String option2;
+        do {
+            option2 = JOptionPane.showInputDialog(null, """
+
+                        SPECIALTIES MENU
+
+                    1. Show all Specialties
+                    2. Add Specialty
+                    3. Update Specialty
+                    4. Delete Specialty
+                    5. Show all Physicians in a Specialty
+                    6. Exit
+
+                    Choose an option:
+
+                    """);
+            switch (option2) {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    JOptionPane.showMessageDialog(null, "Going back to main menu");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Not a valid option");
+                    break;
+            }
+        } while (!option2.equals("6"));
+    }
+
+    public void passengerMenu() {
+        String option2;
+        do {
+            option2 = JOptionPane.showInputDialog(null, """
+
+                        SPECIALTIES MENU
+
+                    1. Show all Specialties
+                    2. Add Specialty
+                    3. Update Specialty
+                    4. Delete Specialty
+                    5. Show all Physicians in a Specialty
+                    6. Exit
+
+                    Choose an option:
+
+                    """);
+            switch (option2) {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    JOptionPane.showMessageDialog(null, "Going back to main menu");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Not a valid option");
+                    break;
+            }
+        } while (!option2.equals("6"));
+    }
+
+    public void reservationMenu() {
+        String option2;
+        do {
+            option2 = JOptionPane.showInputDialog(null, """
+
+                        SPECIALTIES MENU
+
+                    1. Show all Specialties
+                    2. Add Specialty
+                    3. Update Specialty
+                    4. Delete Specialty
+                    5. Show all Physicians in a Specialty
+                    6. Exit
+
+                    Choose an option:
+
+                    """);
+            switch (option2) {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    JOptionPane.showMessageDialog(null, "Going back to main menu");
+                    break;
+                default:
+                    JOptionPane.showMessageDialog(null, "Not a valid option");
+                    break;
+            }
+        } while (!option2.equals("6"));
+    }
 }
